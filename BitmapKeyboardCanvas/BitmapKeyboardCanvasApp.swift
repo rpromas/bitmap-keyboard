8a5e53d372bc41c6ca43cb4b15d3645c81cee698
Romas <romas@sentante.com>
2026-04-01 13:50:36 +0300

    Initial Commit

---
+++ BitmapKeyboardCanvas/BitmapKeyboardCanvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitmapKeyboardCanvasApp.swift
+//  BitmapKeyboardCanvas
+//
+//  Created by Romas on 01/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct BitmapKeyboardCanvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BitmapKeyboardCanvas/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BitmapKeyboardCanvas
+//
+//  Created by Romas on 01/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
